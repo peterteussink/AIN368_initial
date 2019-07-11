a76898dcaad4bde76308f59060af4c1d0066ec16
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2019-07-11 16:00:34 +0200

    initial checkin

---
+++ webcomponent.js
@@ -0,0 +1,41 @@
+(function()  {
+    let d3Script = document.createElement('script');
+    d3Script.src = 'https://d3js.org/d3.v5.min.js';
+    d3Script.async = false;
+    document.head.appendChild(d3Script);
+
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+      <style>
+      </style>
+    `;
+
+    d3Script.onload = () => 
+
+    customElements.define('com-sap-teched-gauge-XX', class Gauge extends HTMLElement {
+
+
+        disconnectedCallback () {
+            // your cleanup code goes here
+            try{
+                document.head.removeChild(d3Script);
+            }
+            catch{}
+        }
+    
+        constructor() {
+            super();
+            //Constants
+            if (!window._d3){
+                window._d3 = d3;
+            }
+            
+            this._shadowRoot = this.attachShadow({mode: 'open'});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._svgContainer;
+        };
+    
+    
+    });
+        
+})();
